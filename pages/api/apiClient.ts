45b9d60ed2dd81bbced467b1e8dab5310f8e0538
KhangTT <Khangtt@rikkeisoft.com>
2022-08-03 14:38:55 +0700

    Setup api

---
+++ pages/api/apiClient.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+import apiConfig from "./apiConfig";
+
+const axiosClient = axios.create({
+    baseURL: apiConfig.baseUrl,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
--- pages/api/apiConfig.ts
+++ pages/api/apiConfig.ts
@@ -1,8 +1,8 @@
 const apiConfig = {
-    baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'd29a76ce6b58ee5cface1d2a11323401',
-    originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
-    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
+    baseUrl: "https://api.themoviedb.org/3/",
+    apiKey: "d29a76ce6b58ee5cface1d2a11323401",
+    originalImage: (imgPath: string) => `https://image.tmdb.org/t/p/original/${imgPath}`,
+    w500Image: (imgPath: string) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 };
 
 export default apiConfig;
