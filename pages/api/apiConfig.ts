1baf3308554a0d9c91c1f348bec9f2e457312e23
KhangTT <Khangtt@rikkeisoft.com>
2022-08-03 14:29:46 +0700

    Setup style global

---
+++ pages/api/apiConfig.ts
@@ -0,0 +1,8 @@
+const apiConfig = {
+    baseUrl: 'https://api.themoviedb.org/3/',
+    apiKey: 'd29a76ce6b58ee5cface1d2a11323401',
+    originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
+    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
+};
+
+export default apiConfig;
