e69310f71e9ebc1eaa9cf981d18b24d9257f8e26
KhangTT <Khangtt@rikkeisoft.com>
2022-08-04 09:40:31 +0700

    update header layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,67 +1,30 @@
-import * as React from "react";
-import { styled, alpha } from "@mui/material/styles";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import InputBase from "@mui/material/InputBase";
-import Badge from "@mui/material/Badge";
-import MenuItem from "@mui/material/MenuItem";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
-import SearchIcon from "@mui/icons-material/Search";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import MailIcon from "@mui/icons-material/Mail";
+import MenuIcon from "@mui/icons-material/Menu";
 import NotificationsIcon from "@mui/icons-material/Notifications";
-import MoreIcon from "@mui/icons-material/MoreVert";
+import SearchIcon from "@mui/icons-material/Search";
+import { Divider, Drawer, List, ListItem, ListItemButton, ListItemText } from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import Badge from "@mui/material/Badge";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import InputBase from "@mui/material/InputBase";
+import { styled } from "@mui/material/styles";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 import { useRouter } from "next/router";
+import * as React from "react";
+import { useState } from "react";
 import { PROFILE_PATH } from "../models";
 
-const Search = styled("div")(({ theme }) => ({
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    "&:hover": {
-        backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-        marginLeft: theme.spacing(3),
-        width: "auto",
-    },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: "inherit",
-    "& .MuiInputBase-input": {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-        transition: theme.transitions.create("width"),
-        width: "100%",
-        [theme.breakpoints.up("md")]: {
-            width: "20ch",
-        },
-    },
-}));
+import styles from "./header.module.scss";
 
 export default function Header() {
     const router = useRouter();
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
     const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState<null | HTMLElement>(null);
+    const [menuPopup, setMenuPopup] = useState(false);
+    const navItems = ["Home", "About", "Contact"];
 
     const isMenuOpen = Boolean(anchorEl);
     const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
@@ -79,31 +42,35 @@ export default function Header() {
         handleMobileMenuClose();
     };
 
+    const handleMenuClick = () => {
+        setMenuPopup(!menuPopup);
+    };
+
+    const navbarWidth = 200;
+
+    const navbar = (
+        <Box onClick={handleMenuClick} className={styles.navbar}>
+            <Typography variant="h6" className={styles.title}>
+                Movies App
+            </Typography>
+            <Divider />
+            <List>
+                {navItems.map((item) => (
+                    <ListItem key={item} disablePadding>
+                        <ListItemButton className={styles.item}>
+                            <ListItemText primary={item} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    );
+
     const handleProfileClick = (event: React.MouseEvent<HTMLElement>) => {
         router.push(`${PROFILE_PATH}${Math.random().toString}`);
     };
 
     const menuId = "primary-search-account-menu";
-    const renderMenu = (
-        <Menu
-            anchorEl={anchorEl}
-            anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-            }}
-            id={menuId}
-            keepMounted
-            transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-            }}
-            open={isMenuOpen}
-            onClose={handleMenuClose}
-        >
-            <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-            <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-        </Menu>
-    );
 
     const mobileMenuId = "primary-search-account-menu-mobile";
 
@@ -117,6 +84,7 @@ export default function Header() {
                         color="inherit"
                         aria-label="open drawer"
                         sx={{ mr: 2 }}
+                        onClick={handleMenuClick}
                     >
                         <MenuIcon />
                     </IconButton>
@@ -128,16 +96,37 @@ export default function Header() {
                     >
                         MUI
                     </Typography>
-                    <Search>
-                        <SearchIconWrapper>
+                    <div className={styles["search-wrapper"]}>
+                        <div className={styles["search-input"]}>
                             <SearchIcon />
-                        </SearchIconWrapper>
-                        <StyledInputBase
+                        </div>
+                        <InputBase
                             placeholder="Search…"
                             inputProps={{ "aria-label": "search" }}
+                            className={styles["search-placeholder"]}
                         />
-                    </Search>
+                    </div>
                     <Box sx={{ flexGrow: 1 }} />
+                    {/* navbar */}
+                    <Box component="nav">
+                        <Drawer
+                            variant="temporary"
+                            open={menuPopup}
+                            onClose={handleMenuClick}
+                            ModalProps={{
+                                keepMounted: true, // Better open performance on mobile.
+                            }}
+                            sx={{
+                                display: { xs: "block", sm: "none" },
+                                "& .MuiDrawer-paper": {
+                                    boxSizing: "border-box",
+                                    width: navbarWidth,
+                                },
+                            }}
+                        >
+                            {navbar}
+                        </Drawer>
+                    </Box>
                     <Box sx={{ display: { xs: "none", md: "flex" } }}>
                         <IconButton size="large" aria-label="show 4 new mails" color="inherit">
                             <Badge badgeContent={4} color="error">
