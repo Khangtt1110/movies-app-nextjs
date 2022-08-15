d1a4ea97b8b432b6aef5ce6a9db24d22846ac305
KhangTT <Khangtt@rikkeisoft.com>
2022-08-15 16:15:56 +0700

    update slice component response

---
+++ components/button/button.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./button.module.scss";
+type Props = {
+    text: string;
+    onClick: () => void;
+};
+
+const Button = (props: Props) => {
+    return (
+        <button onClick={props.onClick} className={`px-5 py-3 ${styles.button}`}>
+            {props.text}
+        </button>
+    );
+};
+
+export default Button;
--- components/slice/slice.tsx
+++ components/slice/slice.tsx
@@ -1,7 +1,5 @@
-import { CircularProgress, Rating, Typography } from "@mui/material";
-import Image from "next/image";
+import { CircularProgress, Typography } from "@mui/material";
 import { useRouter } from "next/router";
-import { Swiper, SwiperSlide } from "swiper/react";
 import {
     getAnotherName,
     getLastName,
@@ -13,11 +11,14 @@ import { useAppDispatch } from "../../features/hooks";
 import { setDetailState } from "../../features/movie/movieSlice";
 import apiConfig from "../../pages/api/apiConfig";
 
-import { Autoplay, Lazy, Pagination } from "swiper";
-import { Category, CategoryData, Genres, MOVIES_PATH, TV_SHOW_PATH } from "../../models";
-import styles from "./slice.module.scss";
 import { useEffect, useState } from "react";
+import { Category, CategoryData, Genres, MOVIES_PATH, TV_SHOW_PATH } from "../../models";
 import categoryApi from "../../pages/api/categoryApi";
+import styles from "./slice.module.scss";
+import Button from "../button";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Lazy, Pagination } from "swiper";
+import "swiper/css";
 
 type Props = {
     data: CategoryData[];
@@ -28,6 +29,7 @@ const Slice = (props: Props) => {
     const [genres, setGenres] = useState<Genres>();
     const dispatch = useAppDispatch();
     const router = useRouter();
+
     const path = props.cate === Category.movie ? MOVIES_PATH : TV_SHOW_PATH;
 
     // get background path and set opacity
@@ -61,66 +63,95 @@ const Slice = (props: Props) => {
             { shallow: true },
         );
     };
-
+    // Get all genres
     useEffect(() => {
         const getGenres = async () => {
             const response = await categoryApi.getGenres();
             setGenres(response);
         };
-
         getGenres();
     }, []);
 
-    console.log(genres);
+    const getGenreById = (id: number) => {
+        return genres?.genres.find((x) => x.id === id)?.name + " ";
+    };
 
     return (
-        <Typography className={`col-12 ${styles.container}`}>
+        <Swiper
+            slidesPerView={1}
+            spaceBetween={0}
+            lazy={{ loadPrevNext: true }}
+            loop={true}
+            autoplay={{ delay: 5000 }}
+            pagination={{
+                clickable: true,
+            }}
+            modules={[Pagination, Autoplay, Lazy]}
+            className={styles.container}
+        >
             {props.data.map((item) => (
-                <Typography
-                    key={item.id}
-                    style={{ background: background(item.backdrop_path, item.poster_path) }}
-                    className={`${styles.background}`}
-                >
-                    <Typography className="container">
-                        <Typography className={styles.title}>NEW RELEASES</Typography>
+                <SwiperSlide key={item.id}>
+                    <Typography
+                        key={item.id}
+                        style={{ background: background(item.backdrop_path, item.poster_path) }}
+                        className={`${styles.background}`}
+                    >
+                        <Typography className="container">
+                            <Typography className={styles.title}>NEW RELEASES</Typography>
 
-                        <div
-                            className={`row  ${styles.name}`}
-                            onClick={() => {
-                                handleSliceClick(item.id, item.title);
-                            }}
-                        >
-                            <Typography className={`fw-bold col-12 display-1`}>
-                                {getAnotherName(item.title)}
-                            </Typography>
-                            <Typography className={`fw-bold col-12 display-1`}>
-                                {getLastName(item.title)}
-                            </Typography>
-                        </div>
-
-                        <Typography className={`row d-flex ${styles.description}`}>
-                            <Typography className={`col-md-12 col-lg-3 d-flex ${styles.rating}`}>
-                                <CircularProgress
-                                    variant="determinate"
-                                    value={item.vote_average * 10}
-                                    color={"success"}
-                                />
-                                <Typography className={`${styles.average}`}>
-                                    {totalRate(item.vote_average)}
+                            <div className={`row  ${styles.name}`}>
+                                <Typography className={`fw-bold col-12 display-1`}>
+                                    {getAnotherName(item.title)}
                                 </Typography>
-                                IMDB SCORE
+                                <Typography className={`fw-bold col-12 display-1`}>
+                                    {getLastName(item.title)}
+                                </Typography>
+                            </div>
+
+                            <Typography className={`row d-flex ${styles.description}`}>
+                                <Typography
+                                    className={`col-md-12 col-lg-3 d-flex ${styles.rating}`}
+                                >
+                                    <CircularProgress
+                                        variant="determinate"
+                                        value={item.vote_average * 10}
+                                        color={"success"}
+                                    />
+                                    <Typography className={`${styles.average}`}>
+                                        {totalRate(item.vote_average)}
+                                    </Typography>
+                                    IMDB SCORE
+                                </Typography>
+
+                                <Typography className={`col-md-12 col-lg-2`}>
+                                    {stringToDate(item.release_date)}
+                                </Typography>
+
+                                <Typography
+                                    className={`col-md-12 col-lg-7 d-flex ${styles.genres}`}
+                                >
+                                    {item.genre_ids.map((id, index) =>
+                                        index < 3 ? (
+                                            <Typography key={id}>{getGenreById(id)}</Typography>
+                                        ) : null,
+                                    )}
+                                </Typography>
+                            </Typography>
+                            <Typography className="col-md-12 col-lg-6 my-3">
+                                {overText(item.overview, 200)}
                             </Typography>
 
-                            <Typography className="col-md-12 col-lg-2">
-                                {stringToDate(item.release_date)}
-                            </Typography>
+                            <Button
+                                text="SEE MORE"
+                                onClick={() => {
+                                    handleSliceClick(item.id, item.title);
+                                }}
+                            />
                         </Typography>
-                        <Typography></Typography>
                     </Typography>
-                </Typography>
+                </SwiperSlide>
             ))}
-            <div></div>
-        </Typography>
+        </Swiper>
     );
 };
 
